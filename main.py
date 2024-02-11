29857a24e2bb742fce4a25113d1672e1c14d2178
jas387 <jhonatas387@gmail.com>
2024-02-11 14:17:18 -0300

    bug fixes in file menu

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ class App:
 		menu_preference = MenuPreference()
 
 		menus = [menu_file,menu_edit,menu_view,menu_preference]
-		self._top_menu = flet.MenuBar(expand=False,controls=menus)
+		self._top_menu = flet.MenuBar(expand=True,controls=menus)
 		self._page.controls.append(flet.Row([self._top_menu],alignment=flet.MainAxisAlignment.CENTER))
 		# tabs
 		self._editor = Editor()
@@ -96,29 +96,27 @@ class App:
 		menu_preference._on_pluggins.bind(on_preference_pluggin)
 
 		# keyboard shortcuts
-		## File Menu
+		# File Menu
 		Shortcut.register('N', callback=on_file_new)
 		Shortcut.register('O', callback=on_file_open)
 		Shortcut.register('S', ctrl=True, shift=False, callback=on_file_save)
 		Shortcut.register('S', ctrl=True, shift=True, callback=on_file_save_as)
 		Shortcut.register('W', callback=on_file_close)
-		## View Menu
+		# View Menu
 		Shortcut.register(']', callback=on_view_zoom_in)
 		Shortcut.register('[', callback=on_view_zoom_out)
 		Shortcut.register('=', callback=on_view_zoom_reset)
 		Shortcut.register('G', callback=on_view_go_to)
 
-		## Tab Navegation
+		# Tab Navegation
 		Shortcut.register('Arrow Right', ctrl=False,alt=True,callback=Callback(self._editor.next_tab))
 		Shortcut.register('Arrow Left', ctrl=False,alt=True,callback=Callback(self._editor.prev_tab))
-		Shortcut.register('Arrow Up', ctrl=False,alt=True,callback=Callback(self._editor.go_first_tab)) #first
-		Shortcut.register('Arrow Down', ctrl=False,alt=True,callback=Callback(self._editor.go_last_tab)) #last
+		Shortcut.register('Arrow Up', ctrl=False,alt=True,callback=Callback(self._editor.go_first_tab))  # first
+		Shortcut.register('Arrow Down', ctrl=False,alt=True,callback=Callback(self._editor.go_last_tab))  # last
 		for digit in range(1,11):
 			if digit==10:
 				digit =0
-			Shortcut.register(str(digit),ctrl=False, alt=True,callback=Callback(self._editor.go_tab, index=digit-1 if digit!=0 else 9))
-
-
+			Shortcut.register(str(digit),ctrl=False, alt=True,callback=Callback(self._editor.go_tab, index=digit - 1 if digit != 0 else 9))
 
 		# status bar
 		self._status_bar = flet.Row([flet.Container(content=flet.Text('status bar:', expand=True),expand=True, bgcolor='grey')])
@@ -141,12 +139,9 @@ class Callback:
 		if self._bind is not None:
 			self._bind.call()
 
-
 	def bind(self, callback):
 		self._bind = callback
 	
-		
-		
 
 class Shortcut:
 	__SHORTCUTS = []
@@ -177,7 +172,6 @@ class Shortcut:
 			return False
 		else:
 			return True
-
 
 	@classmethod
 	def on_keyboard_event(cls, event: flet.KeyboardEvent):
@@ -257,7 +251,7 @@ class Editor(flet.UserControl):
 			content=flet.Text('you want save you file before close?'),
 			actions=[
 				flet.ElevatedButton('yes',on_click=on_close),flet.ElevatedButton('no',on_click=on_close),flet.ElevatedButton('cancel',on_click=on_close)
-			])
+		])
 
 		self.page.dialog = dialog
 		dialog.open = True
@@ -271,24 +265,32 @@ class Editor(flet.UserControl):
 		self._tabs.selected_index = index - 1 if index != 0 else index
 		self._tabs.tabs.pop(index)
 		self.update()
+	
 	def active_tab(self):
 		if len(self._tabs.tabs)>0:
 			return self._tabs.tabs[self._tabs.selected_index]
 		return None
+	
 	def next_tab(self):
 		index = self._tabs.selected_index
-		self.go_tab(index+1)
+		self.go_tab(index + 1)
+	
 	def prev_tab(self):
 		index = self._tabs.selected_index
-		self.go_tab(index-1)
+		self.go_tab(index - 1)
+	
 	def go_tab(self, index: int=0):
-		self._tabs.selected_index = index
-		self._tabs.update()
-		self.active_tab()._textfield.focus()
+		tabs = len(self._tabs.tabs)
+		if index>=0 and index<tabs:
+			self._tabs.selected_index = index
+			self._tabs.update()
+			self.active_tab()._textfield.focus()
+	
 	def go_first_tab(self):
 		self.go_tab(0)
+	
 	def go_last_tab(self):
-		self.go_tab(len(self._tabs.tabs)-1)
+		self.go_tab(len(self._tabs.tabs) - 1)
 
 	# file operations
 	def file_open(self):
@@ -336,6 +338,12 @@ class Editor(flet.UserControl):
 		tab = self.active_tab()
 		if tab is None:
 			return
+		if self.page.platform=='android':
+			self._save_as_android(tab,after_save=after_save)
+		else:
+			self._save_as_desktop(tab,after_save=after_save)
+	
+	def _save_as_desktop(self,tab,after_save: callable=None):
 		def on_result(e):
 			if e.path:
 				path = e.path
@@ -351,15 +359,38 @@ class Editor(flet.UserControl):
 		self.page.update()
 		dialog.save_file()
 
+	def _write_to_file(self, path, data):
+		with open(path,'w') as file:
+			file.write(data)
+
+	def _save_as_android(self,tab,after_save: callable=None):
+		def on_result(e):
+			if e.path:
+				path = e.path
+				filename = e.name
+				folder =e.folder
+				print('path:',path,'filename:',filename,'folder:',folder)
+				tab.set_filename(filename)
+				tab.set_path(path)
+				with open(path,'w') as file:
+					file.write(tab._textfield.value)
+				if after_save is not None:
+					after_save()
+		picker = AndroidFilePicker(tab,on_result=on_result)
+
 	# edit menu
 	def edit_copy(self):
 		pass
+	
 	def edit_cut(self):
 		pass
+	
 	def edit_paste(self):
 		pass
+	
 	def edit_find(self):
 		pass
+	
 	def edit_replace(self):
 		pass
 
@@ -378,34 +409,41 @@ class Editor(flet.UserControl):
 
 	def view_zoom_in(self):
 		self._view_zoom(2)
+	
 	def view_zoom_out(self):
 		self._view_zoom(-2)
+	
 	def view_zoom_reset(self):
 		self._view_zoom()
+	
 	def view_word_wrap(self):
 		pass
 
 	def view_line_number(self):
 		pass
+	
 	def view_go_to(self):
 		tab = self.active_tab()
 		if tab is None:
 			return
+		
 		def on_change(event):
 			go_button.disabled = len(textfield.value)<=0
 			go_button.update()
+		
 		def on_submit(event):
 			_input=int(textfield.value)
 			if _input>=0 and _input<=len(tab._data):
 				# do scroll thing
 				pass
 			close()
+		
 		def close():
 			dialog.open = False
 			self.page.update()
 		
 		only_filter = flet.InputFilter('^[0-9]*')
-		textfield = flet.TextField(on_submit=on_submit,keyboard_type=flet.KeyboardType.NUMBER,input_filter=only_filter,on_change=on_change)
+		textfield = flet.TextField(on_submit=on_submit,keyboard_type=flet.KeyboardType.NONE,input_filter=only_filter,on_change=on_change)
 		view = flet.SafeArea(expand=True,content=textfield)
 
 		go_button = flet.IconButton(icon=flet.icons.FOLLOW_THE_SIGNS,on_click=on_submit,disabled=True)
@@ -419,19 +457,148 @@ class Editor(flet.UserControl):
 	# preference
 	def preference_setting(self):
 		pass
+	
 	def preference_shortcut(self):
 		pass
+	
 	def preference_theme(self):
 		pass
+	
 	def preference_syntax_highlight(self):
 		pass
+	
 	def preference_font(self):
 		pass
+	
 	def preference_pluggin(self):
 		pass
 
 
+class AndroidFilePickerResult:
+	def __init__(self, path: str=None, name: str=None, folder: str=None):
+		self.path = path
+		self.name = name
+		self.folder = folder
 
+
+class AndroidFilePicker:
+	def __init__(self, tab: flet.Tab, on_result: callable=None):
+		self._folder = None
+		self._filename = None
+		self._tab = tab
+		self._page = self._tab.page
+		self._on_result = on_result
+		self._filename_textfield = flet.Ref[flet.TextField]()
+		self._dismiss_dialog = flet.Ref[flet.AlertDialog]()
+		self._filename_dialog = flet.Ref[flet.AlertDialog]()
+		self._overwrite_dialog = flet.Ref[flet.AlertDialog]()
+
+		self.__get_folder()
+
+	def __on_result(self):
+		if self._on_result is not None:
+			if self._folder is None or self._filename is None:
+				_path = None
+			else:
+				_path = f'{self._folder}/{self._filename}'
+			result = AndroidFilePickerResult(path=_path,name=self._filename,folder=self._folder)
+			self._on_result(result)
+
+	def __get_folder(self):
+		def on_result(e):
+			if e.path:
+				self._folder = e.path
+				self.__get_filename()
+
+		picker = flet.FilePicker(on_result=on_result)
+		self._page.overlay.append(picker)
+		self._page.update()
+		picker.get_directory_path()
+
+	def __open_dialog(self, dialog: flet.AlertDialog):
+		self._page.dialog = dialog
+		dialog.open = True
+		self._page.update()
+
+	def __close_dialog(self, dialog: flet.AlertDialog):
+		if dialog is not None:
+			dialog.open = False
+		if self._page.dialog is not None:
+			self._page.dialog.open = False
+		self._page.update()
+
+	def __get_filename(self):
+		title = flet.Text(value='filename',text_align=flet.TextAlign.CENTER)
+		textfield = flet.TextField(ref=self._filename_textfield,keyboard_type=flet.KeyboardType.NONE,
+			on_change=self.__on_change_filename,on_submit=self.__on_submit_filename)
+		button = flet.IconButton(icon=flet.icons.SAVE,on_click=self.__on_submit_filename)
+		dialog = flet.AlertDialog(ref=self._filename_dialog,title=title,content=textfield,
+			actions=[button],actions_alignment=flet.MainAxisAlignment.CENTER,on_dismiss=self.__on_dismiss_filename)
+		self.__open_dialog(dialog)
+		textfield.focus()
+
+	def __on_change_filename(self, event):
+		if self.__file_exists(event.data):
+			event.control.error_text = 'file already exists!'
+		else:
+			event.control.error_text = None
+		event.control.update()
+
+	def __file_exists(self, filename:str):
+		return os.path.isfile(f'{self._folder}/{filename}')
+	
+	def __on_dismiss_filename(self, event):
+		self.__close_dialog(self._filename_dialog.current)
+		self.__on_result()
+
+	def __old_on_dismiss_filename(self, event):
+		self.__close_dialog(event.control)
+		title = flet.Text(value='cancel?',text_align=flet.TextAlign.CENTER)
+		yes = flet.ElevatedButton(text='yes',on_click=self.__on_click_yes_cancel)
+		no = flet.ElevatedButton(text='no',on_click=self.__on_click_no_cancel)
+		dialog = flet.AlertDialog(ref=self._dismiss_dialog,title=title,content=flet.Text('cancel without save?',text_align=flet.TextAlign.CENTER),
+			actions=[yes,no],actions_alignment=flet.MainAxisAlignment.CENTER,on_dismiss=self.__on_click_yes_cancel)
+		self.__open_dialog(dialog)
+	
+	def __on_click_yes_cancel(self, event):
+		self.__close_dialog(self._dismiss_dialog.current)
+		self.__close_dialog(self._filename_dialog.current)
+		self.__on_result()
+
+	def __on_click_no_cancel(self, event):
+		self.__close_dialog(self._dismiss_dialog)
+		self.__close_dialog(self._filename_dialog)
+		self.__get_filename()
+
+	def __on_submit_filename(self, event):
+		self._filename = self._filename_textfield.current.value
+		self.__close_dialog(self._filename_dialog.current)
+		if self.__file_exists(self._filename):
+			self.__get_overwrite()
+		else:
+			self.__on_result()
+
+	def __get_overwrite(self):
+		yes = flet.ElevatedButton(text='yes',on_click=self.__on_click_yes_overwrite)
+		no = flet.ElevatedButton(text='no',on_click=self.__on_click_no_overwrite)
+		cancel = flet.ElevatedButton(text='cancel',on_click=self.__on_click_cancel_overwrite)
+		title = flet.Text(value='overwrite?',text_align=flet.TextAlign.CENTER)
+		dialog = flet.AlertDialog(ref=self._overwrite_dialog,title=title,content=flet.Text(f'overwrite file {self._filename}?',text_align=flet.TextAlign.CENTER),
+			actions=[yes,no],actions_alignment=flet.MainAxisAlignment.CENTER,on_dismiss=self.__on_click_cancel_overwrite)
+		self.__open_dialog(dialog)
+
+	def __on_click_yes_overwrite(self, event):
+		self.__close_dialog(self._overwrite_dialog.current)
+		self.__on_result()
+	
+	def __on_click_no_overwrite(self, event):
+		self.__close_dialog(self._overwrite_dialog.current)
+		self.__get_filename()
+		self.__on_result()
+	
+	def __on_click_cancel_overwrite(self, event):
+		self.__close_dialog(self._overwrite_dialog.current)
+		self.__on_result()
 
 
 class Tab(flet.Tab):
@@ -445,7 +612,7 @@ class Tab(flet.Tab):
 		self._size = size if size>0 else len(data)
 		# flet
 		
-		self._textfield = flet.TextField(multiline=True,min_lines=1000,expand=True,value=data, on_change=self._on_textfield_change)
+		self._textfield = flet.TextField(keyboard_type=flet.KeyboardType.NONE,multiline=True,min_lines=1000,expand=True,value=data, on_change=self._on_textfield_change)
 		self._tab_content = flet.Tooltip(message=f'size: {size_fmt(self._size)}\npath: {path}\nchars: {self._size}', content=flet.Text(self._title))
 		self.tab_content=self._tab_content
 		self.content=self._textfield
@@ -471,7 +638,6 @@ class Tab(flet.Tab):
 		self._filename = value
 		self._title = value
 		self._update_tooltip()
-
 
 
 class TextField(flet.TextField):
@@ -578,7 +744,6 @@ class MenuPreference(Menu):
 		self.add_button('Pluggins', flet.icons.POWER, self._on_pluggins)
 
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
 	app = App()
 	flet.app(target=app.target)
